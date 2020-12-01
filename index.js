951a8dde7b7df4e2eb901ffd5faa640306012eae
splendid-process-7143 <usmonovjafar@gmail.com>
2020-12-01 02:21:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.console.error("Test");
-console.console.log("are you okay");
-console.console.warn("do not do it man");
+console.console.log('are you okay');
+console.console.warn('do not do it man');
